f015fbe812afece5020beb78b252df0ee9405f2d
cristi <cristiandanielboabes@gmail.com>
2024-08-20 17:00:53 +0300

    Final version of phase 1

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x,y,width,height, controlType, maxSpeed=2.5){
+    constructor(x,y,width,height,controlType,maxSpeed=3,color="blue"){
         this.x=x;
         this.y=y;
         this.width=width;
@@ -12,32 +12,46 @@ class Car{
         this.angle=0;
         this.damaged=false;
 
-        this.useBrain = controlType=="AI";
+        this.useBrain=controlType=="AI";
 
         if(controlType!="DUMMY"){
             this.sensor=new Sensor(this);
-            this.brain = new NeuralNetwork(
-                [this.sensor.rayCount, 6, 4]
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
             );
         }
         this.controls=new Controls(controlType);
+
+        this.img=new Image();
+        this.img.src="car.png"
+
+        this.mask=document.createElement("canvas");
+        this.mask.width=width;
+        this.mask.height=height;
+
+        const maskCtx=this.mask.getContext("2d");
+        this.img.onload=()=>{
+            maskCtx.fillStyle=color;
+            maskCtx.rect(0,0,this.width,this.height);
+            maskCtx.fill();
+
+            maskCtx.globalCompositeOperation="destination-atop";
+            maskCtx.drawImage(this.img,0,0,this.width,this.height);
+        }
     }
 
-    update(roadBorders, traffic){
+    update(roadBorders,traffic){
         if(!this.damaged){
             this.#move();
-            this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders, traffic);
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders,traffic);
         }
         if(this.sensor){
-            this.sensor.update(roadBorders, traffic);
-            const offset = this.sensor.readings.map(
+            this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
                 s=>s==null?0:1-s.offset
             );
-            const outputs = NeuralNetwork.feedForward(
-                offset, this.brain
-            );
-            // console.log(outputs);
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
 
             if(this.useBrain){
                 this.controls.forward=outputs[0];
@@ -48,7 +62,7 @@ class Car{
         }
     }
 
-    #assessDamage(roadBorders, traffic){
+    #assessDamage(roadBorders,traffic){
         for(let i=0;i<roadBorders.length;i++){
             if(polygonIntersect(this.polygon,roadBorders[i])){
                 return true;
@@ -64,8 +78,8 @@ class Car{
 
     #createPolygon(){
         const points=[];
-        const rad = Math.hypot(this.width,this.height)/2;
-        const alpha = Math.atan2(this.width,this.height);
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
         points.push({
             x:this.x-Math.sin(this.angle-alpha)*rad,
             y:this.y-Math.cos(this.angle-alpha)*rad
@@ -75,14 +89,13 @@ class Car{
             y:this.y-Math.cos(this.angle+alpha)*rad
         });
         points.push({
-            x:this.x-Math.sin( Math.PI + this.angle-alpha)*rad,
-            y:this.y-Math.cos( Math.PI + this.angle-alpha)*rad
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
         });
         points.push({
-            x:this.x-Math.sin( Math.PI + this.angle+alpha)*rad,
-            y:this.y-Math.cos( Math.PI + this.angle+alpha)*rad
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
         });
-
         return points;
     }
 
@@ -125,22 +138,28 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx, color){
-        if(this.damaged){
-            ctx.fillStyle='gray';
-        }else{
-            ctx.fillStyle=color;
-        }
-        ctx.beginPath();
-        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
-
-        for(let i=1;i<this.polygon.length;i++){
-            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
-        }
-        ctx.fill();
-
-        if(this.sensor){
+    draw(ctx,drawSensor=false){
+        if(this.sensor && drawSensor){
             this.sensor.draw(ctx);
         }
+
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+        if(!this.damaged){
+            ctx.drawImage(this.mask,
+                -this.width/2,
+                -this.height/2,
+                this.width,
+                this.height);
+            ctx.globalCompositeOperation="multiply";
+        }
+        ctx.drawImage(this.img,
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height);
+        ctx.restore();
+
     }
 }
